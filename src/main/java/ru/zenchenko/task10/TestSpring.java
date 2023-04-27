6ebd1075a02f48a790f5188195516885f2bb647d
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2023-04-27 10:25:24 +0300

    comments

--- src/main/java/ru/zenchenko/task10/TestSpring.java
+++ src/main/java/ru/zenchenko/task10/TestSpring.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class TestSpring {
     public static void main(String[] args) {
+        //Берем контекст
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
-
         //Берем бин
         SortingAlgorythm algorythm = context.getBean(args[0], SortingAlgorythm.class);
-
+        //Метод сортировки
         algorythm.doSort();
-
+        //Закрываем контекст
         context.close();
     }
 }
